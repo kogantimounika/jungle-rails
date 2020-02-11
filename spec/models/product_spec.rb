d9d4afcb05c6c2583eedbb65206a4aa678fd675f
kogantimounika <kogantimounica@gmail.com>
2020-02-11 01:13:30 +0000

    Added test cases for product

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -6,36 +6,36 @@ RSpec.describe Product, type: :model do
     @category = Category.new(name: "Testing")
     @product = Product.new(name: "Supreme Nas Tee", price_cents: 3500, quantity: 35, :category => @category)
     @product.valid?
-    expect(@product.errors).not_to include("can\'t be blank")
+    expect(@product.errors.full_messages).not_to eql("can\'t be blank")
     end
 
     it 'should not create a product if the name is missing' do
     @category = Category.new(name: "Testing")
     @product = Product.new(price_cents: 3500, quantity: 35, :category => @category)
     @product.valid?
-    expect(@product.errors[:name]).to include("can\'t be blank")
+    expect(@product.errors.full_messages).to eql(["Name can\'t be blank"])
     end
 
     it 'should not create a product if the quantity is missing' do
     @category = Category.new(name: "Testing")
     @product = Product.new(name: "Supreme Nas Tee", price_cents: 3500, :category => @category)
     @product.valid?
-    expect(@product.errors[:quantity]).to include("can\'t be blank")
+    expect(@product.errors.full_messages).to eql(["Quantity can\'t be blank"])
     end
 
     it 'should not create a product if the price is missing' do
     @category = Category.new(name: "Testing")
     @product = Product.new(name: "Supreme Nas Tee", quantity: 35, :category => @category)
     @product.valid?
-    expect(@product.errors[:price]).to include("can\'t be blank")
+    expect(@product.errors.full_messages).to eql(["Price cents is not a number", "Price is not a number", "Price can't be blank"])
     end
 
     it 'should not create a product if the category is missing' do
     @category = Category.new(name: "Testing")
     @product = Product.new(name: "Supreme Nas Tee", price_cents: 3500, quantity: 35)
     @product.valid?
-    expect(@product.errors[:category]).to include("can\'t be blank")
+    expect(@product.errors.full_messages).to eql(["Category can\'t be blank"])
     end
-    
+
   end
 end 
