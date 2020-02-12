cba141fbd881d49aedbed52b64b69e7267f80b6f
kogantimounika <kogantimounica@gmail.com>
2020-02-12 21:46:39 +0000

    Resolved an issue with login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
+  def self.authenticate_with_credentials(email, password)
+    @user = self.where("lower(email) = ?", email.delete(' ').downcase).first.try(:authenticate, password)
+  end
   validates :password, length: { minimum: 5 }
   validates :email, uniqueness: { case_sensitive: false }
 end
